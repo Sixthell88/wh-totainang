c62810a47c64f543985c887797fa6f526116f690
Sixthell88 <thekingmodders@gmail.com>
2026-08-23 16:49:08 +0700

    Update client_dll.cs

--- client_dll.cs
+++ client_dll.cs
@@ -1,10 +1,10 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-04 01:46:09.286657900 UTC
+// 2026-08-20 07:13:18.517249800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
     // Class count: 563
-    // Enum count: 19
+    // Enum count: 14
     public static class ClientDll {
         // Alignment: 4
         // Member count: 5
